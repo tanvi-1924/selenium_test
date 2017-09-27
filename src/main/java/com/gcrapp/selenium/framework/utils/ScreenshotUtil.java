3df3b9a6d2493fc48fc7344666127b9a978e45f3
Tanvi <tanvi.srv.1924@gmail.com>
2017-09-28 02:00:37 +0530

    final

--- src/main/java/com/gcrapp/selenium/framework/utils/ScreenshotUtil.java
+++ src/main/java/com/gcrapp/selenium/framework/utils/ScreenshotUtil.java
@@ -2,6 +2,7 @@ package com.gcrapp.selenium.framework.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Date;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -9,14 +10,43 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import com.gcrapp.selenium.framework.ConfigReaderWriter;
+import com.gcrapp.selenium.tests.utils.AppLogger;
 
 public class ScreenshotUtil {
 
-public static final String SCREENSHOT_LOCATION = ConfigReaderWriter.getProperty("SCREENSHOT_LOCATION");
-
-public static void captureScreenshot(WebDriver driver, String methodName, String testOutcome) throws IOException{
+	public static final String SCREENSHOT_LOCATION = ConfigReaderWriter
+			.getProperty("SCREENSHOT_LOCATION");
 	
-File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-FileUtils.copyFile(srcFile, new File(SCREENSHOT_LOCATION + File.separator + methodName + "_"+ testOutcome + "_" + System.currentTimeMillis() + ".png"));
-}
+	private static AppLogger logger = new AppLogger(ScreenshotUtil.class);
+
+	public static void captureScreenshot(WebDriver driver, String methodName,
+			String testOutcome) throws IOException {
+
+		File srcFile = ((TakesScreenshot) driver)
+				.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(srcFile, new File(getScreenshotFilename(methodName, testOutcome)));
+	}
+	
+	private static String getScreenshotFilename(String methodName, String testOutcome) {
+		logger.info("getScreenshotFilename - Enter with arguments -");
+		final long millis = System.currentTimeMillis();
+		final Date date = new Date(millis);
+		
+		
+		final StringBuilder fileNameBuilder = new StringBuilder();
+		fileNameBuilder.append(SCREENSHOT_LOCATION);
+		fileNameBuilder.append(File.separator);
+		fileNameBuilder.append(methodName);
+		fileNameBuilder.append("_");
+		fileNameBuilder.append(testOutcome);
+		fileNameBuilder.append("_");
+		fileNameBuilder.append(date);
+		fileNameBuilder.append("_");
+		fileNameBuilder.append(getTimeFromMillis(millis));
+		fileNameBuilder.append(".png");
+		return fileNameBuilder.toString();
+	}
+	private static String getTimeFromMillis(long millis){
+		return String.format("%1$tH_%1$tM_%1$tS", millis);
+	}
 }
